f701b342698d9428719e3dcbdf6244bde4c01dab
Ritvik Bharti <bhartiritvik1000@gmail.com>
2025-04-26 16:04:12 +0530

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/home/Home'
 
 function App() {
  
 
   return (
     <>
-     <h1>Hellow World</h1>
+    <Home/>
      
     </>
   )
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+
+
+function Home(){
+    return (
+        <>
+        <h1>Hone Page</h1>
+        </>
+    )
+}
+
+export default Home
